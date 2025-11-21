bc1dbd85c3c3889d9635f9a9a245114936fd310a
Sivamurugan0805 <sivamuruganthangavel5@gmail.com>
2025-11-21 19:58:26 +1000

    Add files via upload

---
+++ Assignments/Assignment5/Project/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app =express();
+const mongoose=require('mongoose')
+
+const userRouter=require('./Router/StudentRouter');
+const port =8000;
+mongoose
+.connect("mongodb+srv://myAtlasDBUser:root@myatlasclusteredu.qk2tejs.mongodb.net/?appName=myAtlasClusterEDU")
+.then(()=>{
+    console.log("connected to DB 🍃");
+})
+.catch((err)=>{
+    console.log(err);
+});
+app.use(express.json());
+
+app.use(userRouter);
+app.listen(port,()=>{
+    console.log(`server running at http://localhost:${port}`);
+
+});
